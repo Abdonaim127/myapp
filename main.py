bc95ddcdd01733e159b194b736ace61a231b461d
abderrazak.naiim <142780594+Abdonaim127@users.noreply.github.com>
2024-06-01 01:50:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,32 +11,65 @@ def format_json(json_data):
 
 
 # Streamlit app
-st.title("Listing Information")
+st.title("Chatgpt Model(β) - Results")
 
-listing_id = st.selectbox("Select Listing ID", options=list(data.WEB_ID.unique()))
+st.write("This app created to help us in evaluating the results of Model")
+#listing_id = st.selectbox("Select Listing ID", options=list(data.WEB_ID.unique()))
+listing_id = st.selectbox("Select Listing ID", options=[5104105,
+5103889,
+5103837,
+5103958,
+5104027,
+5103925,
+5103809,
+5104048,
+5104178])
+
+
 
 if listing_id:
+
     cols = st.columns([1, 1])
 
     with cols[0]:
 
-        st.text_area("Chatgpt Output: ", format_json(data[data.WEB_ID == listing_id].results.iloc[0]), height=400)
-        #st.json(data[data.WEB_ID == listing_id].results.iloc[0])
+        st.text_area("Model Output: ", format_json(data[data.WEB_ID == listing_id].results.iloc[0]), height=400)
+        st.json(data[data.WEB_ID == listing_id].results.iloc[0])
     with cols[1]:
         st.text_area("Listings Details: ", format_json(org_data[org_data.ID == listing_id].to_dict(orient='records')[0]), height=400)
-        #st.json(org_data[org_data.ID == listing_id].to_dict(orient='records')[0])
+        st.json(org_data[org_data.ID == listing_id].to_dict(orient='records')[0])
+
+    st.title('Description')
+
     cols = st.columns([1, 1])
 
     with cols[0]:
-        imgs = org_data[org_data.ID == listing_id].to_dict(orient='records')[0]['IMAGES']
-        for i in imgs:
-            st.image(i['src'], use_column_width=True)
+
+        st.text_area("Generated Description: ", data[data.WEB_ID == listing_id].results.iloc[0]['Description Analysis']['friendly_description'], height=400)
 
     with cols[1]:
-        imgs_res = data[data.WEB_ID == listing_id].results.iloc[0]['Image Analysis']
-        for j,i in enumerate(imgs_res):
-            print(i)
-            st.text_area("Image Analysis " + str(j),
-                         format_json(i), height=222)
-            #st.json(data[i])
+        st.text_area("Original Description: ", org_data[org_data.ID == listing_id].to_dict(orient='records')[0]['DESCRIPTION'], height=400)
+
+
+    imgs = org_data[org_data.ID == listing_id].to_dict(orient='records')[0]['IMAGES']
+    imgs_res = data[data.WEB_ID == listing_id].results.iloc[0]['Image Analysis']
+    st.title('Images')
+    for i in range(len(imgs)):  # Loop to create 10 rows
+
+
+        st.write("Image Analysis " + str(i))
+        cols = st.columns([1, 1])
+
+        with cols[0]:
+
+
+            st.markdown('#')
+            st.image(imgs[i]['src'], use_column_width=True)
+
+        with cols[1]:
+
+
+            st.markdown('#')
+
+            st.json(imgs_res[i])
 
